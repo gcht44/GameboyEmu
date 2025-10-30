3bba55ef55a1d68c75abf733d58d02ae1cc73132
gcht <gab.github44@gmail.com>
2025-10-30 03:03:30 +0100

    Sync cpu at 60 FPS

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> // Ajout de stdlib.h pour malloc et free
+#include <stdlib.h>
 
 #include "../includes/cart.h"
 #include "../includes/cpu.h"
@@ -8,30 +8,51 @@
 #include "../includes/lcd.h"
 #include "../includes/joypad.h"
 
+void handle_key_event(SDL_Event e)
+{
+    if (e.type == SDL_EVENT_KEY_UP)
+    {
+        switch (e.key.key)
+        {
+        case SDLK_Z: update_joypad(K_UP, 0); break;
+        case SDLK_Q: update_joypad(K_LEFT, 0); break;
+        case SDLK_D: update_joypad(K_RIGHT, 0); break;
+        case SDLK_S: update_joypad(K_DOWN, 0); break;
+        case SDLK_A: update_joypad(K_A, 0); break;
+        case SDLK_E: update_joypad(K_B, 0); break;
+        case SDLK_W: update_joypad(K_START, 0); break;
+        case SDLK_X: update_joypad(K_SELECT, 0); break;
+        }
+    }
+    else if (e.type == SDL_EVENT_KEY_DOWN)
+    {
+        switch (e.key.key)
+        {
+        case SDLK_Z: update_joypad(K_UP, 1); break;
+        case SDLK_Q: update_joypad(K_LEFT, 1); break;
+        case SDLK_D: update_joypad(K_RIGHT, 1); break;
+        case SDLK_S: update_joypad(K_DOWN, 1); break;
+        case SDLK_A: update_joypad(K_A, 1); break;
+        case SDLK_E: update_joypad(K_B, 1); break;
+        case SDLK_W: update_joypad(K_START, 1); break;
+        case SDLK_X: update_joypad(K_SELECT, 1); break;
+        }
+    }
+}
+
 int main()
 {
     s_CPU cpu;
     bool play_emu = true;
-    // unsigned lines = 0;
-
-    // Ouvrir le fichier en mode binaire pour éviter les problèmes de traitement de texte
-    /*FILE *fp = fopen("log/log.txt", "wb");
-    if (!fp) {
-        printf("Erreur: Impossible d'ouvrir le fichier de log\n");
-        return -1;
-    }*/
-
-    /*char *buffer = malloc(32768); // 32KB buffer
-    if (buffer) {
-        setvbuf(fp, buffer, _IOFBF, 32768);
-    }*/
-
+    
     const uint32_t TARGET_FPS = 60;
-    const uint32_t FRAME_TIME_MS = 1000 / TARGET_FPS; // ~16.67ms par frame
+    const uint32_t FRAME_TIME_MS = 1000 / TARGET_FPS; // ~16.67ms
+    const uint32_t CYCLES_PER_FRAME = 17556; // M-cycles per frame (70224 T-cycles / 4)
+    
     uint32_t last_frame_time = 0;
+    uint32_t accumulated_cycles = 0;
 
-    load_rom("rom/Tetris (World) (Rev 1).gb");
-    // load_rom("rom/test_roms/instr_timing.gb");
+    load_rom("rom/Legend of Zelda, The - Link's Awakening (France).gb");
     init_cpu(&cpu);  
     init_lcd();
     init_joypad();  
@@ -39,105 +60,48 @@ int main()
 
     while(play_emu)
     {
-        // Écriture dans le fichier log
-        /*fprintf(fp, "%04X:  A:%02x H:%02x L:%02x PCMEM:%02X,%02X,%02X,%02X IF:%02X IE:%02X IME:%d BGP:%02X LCD:%02X STAT:%02X LY:%02X LYC:%02X SCX:%02X SCY:%02X\n",
-            cpu.pc, cpu.a, cpu.h, cpu.l,
-            bus_read(cpu.pc, false), bus_read(cpu.pc+1, false), 
-            bus_read(cpu.pc+2, false), bus_read(cpu.pc+3, false), 
-            bus_read(0xFF0F, false), bus_read(0xFFFF, false), 
-            cpu.ime, bus_read(0xFF47, false), bus_read(0xFF40, false), 
-            bus_read(0xFF41, false), bus_read(0xFF44, false), bus_read(0xFF45, false), bus_read(0xFF43, false), bus_read(0xFF42, false));
-
-
-        if ((++lines & 0x3FF) == 0) {
-            fflush(fp);
-        }*/
         uint32_t current_time = SDL_GetTicks();
-                
-        // Limiter les événements et le rendu à 60 FPS
-        if (current_time - last_frame_time >= FRAME_TIME_MS)
+        
+        while (accumulated_cycles < CYCLES_PER_FRAME) 
         {
-            SDL_Event e;
-            while (SDL_PollEvent(&e)) 
+            // Check event every 256 t-cycles
+            if ((accumulated_cycles & 0xFF) == 0) 
             {
-                if (e.type == SDL_EVENT_QUIT) play_emu = 0;
-                if (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE) play_emu = 0;
-
-                if (e.type == SDL_EVENT_KEY_UP)
+                SDL_Event e;
+                while (SDL_PollEvent(&e)) 
                 {
-                    switch (e.key.key)
-                    {
-                    case SDLK_Z:
-                        update_joypad(K_UP, 0);
-                        break;
-                    case SDLK_Q:
-                        update_joypad(K_LEFT, 0);
-                        break;
-                    case SDLK_D:
-                        update_joypad(K_RIGHT, 0);
-                        break;
-                    case SDLK_S:
-                        update_joypad(K_DOWN, 0);
-                        break;
+                    if (e.type == SDL_EVENT_QUIT) 
+                        play_emu = false;
+                    if (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE) 
+                        play_emu = false;
 
-                    case SDLK_A:
-                        update_joypad(K_A, 0);
-                        break;
-                    case SDLK_E:
-                        update_joypad(K_B, 0);
-                        break;
-                    case SDLK_W:
-                        update_joypad(K_START, 0);
-                        break;
-                    case SDLK_X:
-                        update_joypad(K_SELECT, 0);
-                        break;
-                    default:
-                        break;
-                    }
+                    handle_key_event(e);
                 }
-                if (e.type == SDL_EVENT_KEY_DOWN)
-                {
-                    switch (e.key.key)
-                    {
-                    case SDLK_Z:
-                        update_joypad(K_UP, 1);
-                        break;
-                    case SDLK_Q:
-                        update_joypad(K_LEFT, 1);
-                        break;
-                    case SDLK_D:
-                        update_joypad(K_RIGHT, 1);
-                        break;
-                    case SDLK_S:
-                        update_joypad(K_DOWN, 1);
-                        break;
-
-                    case SDLK_A:
-                        update_joypad(K_A, 1);
-                        break;
-                    case SDLK_E:
-                        update_joypad(K_B, 1);
-                        break;
-                    case SDLK_W:
-                        update_joypad(K_START, 1);
-                        break;
-                    case SDLK_X:
-                        update_joypad(K_SELECT, 1);
-                        break;
-                    default:
-                        break;
-                    }
-                }
+                if (!play_emu) break;
+            }
+            
+            uint8_t cycles = step_emu(&cpu);
+            accumulated_cycles += cycles;
+        }
+        
+        // Reset cycles for the next frame
+        accumulated_cycles -= CYCLES_PER_FRAME;
+        
+        if (current_time - last_frame_time >= FRAME_TIME_MS) 
+        {
+            last_frame_time = current_time;
+        } 
+        else 
+        {
+            // Wait
+            uint32_t sleep_time = FRAME_TIME_MS - (current_time - last_frame_time);
+            if (sleep_time > 0 && sleep_time < FRAME_TIME_MS) 
+            {
+                SDL_Delay(sleep_time);
             }
         }
-        step_emu(&cpu);
     }
 
-    // Nettoyer
-    // fflush(fp);
-    // fclose(fp);
-    // free(buffer);
     SDL_exit();
     return 0;
 }
